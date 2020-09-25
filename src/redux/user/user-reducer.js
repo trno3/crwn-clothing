c2597f80a3bd3c5fb4b9b687160bc996a57bb813
toni <toni_hp@hotmail.com>
2020-09-26 00:39:09 +0200

    Updating app to redirect to home when user is defined

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,10 +1,14 @@
+import userEvent from '@testing-library/user-event';
+import { setCurrentUser } from './user.actions';
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = {
     currentUser: null,
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload,
